a4dcde1dee6b5936cb8b9d5950a0fb901e6a45d9
xmiguelcastillo@gmail.com <xmiguelcastillo@gmail.com>
2025-01-09 13:32:33 -0500

    third commit

--- src/Resume.jsx
+++ src/Resume.jsx
@@ -10,7 +10,7 @@ function PDFViewer() {
       <div className="flex justify-center w-full max-w-[500px] p-4">
         <iframe
           className="bg-[#00ffd7] border border-white rounded-md 
-                 w-[100%] h-[55vh] sm:w-[400px] sm:h-[550px] 
+                 w-[100%] h-[80vh] sm:w-[400px] sm:h-[550px] 
                  md:w-[400px] md:h-[600px] lg:w-[500px] lg:h-[700px]"
           src={pdfUrl}
           title="PDF Viewer"
