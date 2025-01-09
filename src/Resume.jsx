432a5aa978490d687cd5ed171105f56a9c5af609
xmiguelcastillo@gmail.com <xmiguelcastillo@gmail.com>
2025-01-09 13:02:36 -0500

    third commit

--- src/Resume.jsx
+++ src/Resume.jsx
@@ -5,16 +5,12 @@ function PDFViewer() {
   const pdfUrl = "Miguel.pdf";
 
   return (
-    <div className="flex flex-col items-center bg-[#101010] h-full min-h-screen">
+    <div className="flex flex-col items-center bg-[#101010] min-h-screen">
       <HeadingBar />
       <div className="flex justify-center w-full max-w-5xl p-4">
         <iframe
-          className="bg-[#00ffd7] border border-white rounded-md"
+          className="bg-[#00ffd7] border border-white rounded-md w-[500px] sm:w-[400px] sm:h-[550px] lg:w-[500px] lg:h-[700px]"
           src={pdfUrl}
-          style={{
-            width: "65%",
-            aspectRatio: "1 / 1.414", // Maintain A4 aspect ratio (approx. 1:1.414)
-          }}
           title="PDF Viewer"
         />
       </div>
