7b8c14935a9682d73369f5bf1ce88a4c8ba15e84
xmiguelcastillo@gmail.com <xmiguelcastillo@gmail.com>
2025-01-30 17:42:32 -0500

    Initial commit

--- src/components/Languages/JavascriptDisplay.jsx
+++ src/components/Languages/JavascriptDisplay.jsx
@@ -30,20 +30,15 @@ function JavascriptDisplay() {
       <div className="text-white flex justify-center mt-3">
         JavaScript Projects
       </div>
-      <div
-        className="h-[300px] w-[400px]  sm:h-[300px] sm:w-[400px] lg:h-[350px] lg:w-[500px] mt-3 rounded-lg shadow shadow-black border-[1px] border-[#222224]"
-        style={{
-          backgroundSize: "contain",
-          backgroundPosition: "center",
-          backgroundRepeat: "no-repeat",
-        }}
-      >
-        <div className="w-full h-3/5 rounded-lg flex justify-center items-center">
-          {/* Radial Background Effect */}
+      <div className="relative h-[300px] w-[400px] sm:h-[300px] sm:w-[400px] lg:h-[350px] lg:w-[500px] mt-3 rounded-lg shadow shadow-black border-[1px] border-[#222224] overflow-hidden">
+        {/* 3D Model Container */}
+        <div className="relative w-full h-3/5 flex justify-center items-center">
+          {/* Radial Background Effect behind everything */}
           <div
-            className="w-[300px] h-[50px] sm:w-[100px] sm:h-[50px] md:w-[150px] md:h-[80px]  lg:w-[260px]  lg:h-[150px]  rounded-full absolute lg:top-[75%] top-[93%] left-[50%] md:top-[68%] sm:top-[67%] translate-x-[-50%] z-0 translate-y-[-50%] blur-[50px]  pointer-events-none transition-all duration-1000"
+            className="absolute   mt-[90px] flex items-center justify-center w-[290px] h-[100px] pointer-events-none"
             style={{
               background: `radial-gradient(circle at center, ${background} 0%, transparent 100%)`,
+              filter: "blur(70px)",
             }}
           ></div>
 
@@ -67,7 +62,9 @@ function JavascriptDisplay() {
             <OrbitControls enableZoom={true} autoRotate={true} />
           </Canvas>
         </div>
-        <div className="flex-col w-full h-1/5">
+
+        {/* Project Information */}
+        <div className="flex-col  w-full h-1/5">
           <a
             href="https://xmiguelcastillo.github.io/pokedex/bulbasaur"
             target="_blank"
@@ -76,44 +73,28 @@ function JavascriptDisplay() {
           >
             3D Pokedex
           </a>
-          <div className="flex w-full h-full justify-center items-center">
-            <div className="border-[.05px] rounded-sm border-[#8f909c] flex text-white bg-[#111111] w-20 h-5 text-xs mx-2">
-              <div className="w-1/3 flex justify-center text-black items-center h-full bg-[#111111]">
-                <JavascriptIcon />
+          <div className="flex w-full h-full  justify-center items-center">
+            {[
+              { Icon: JavascriptIcon, label: "Javascript" },
+              { Icon: ReactIcon, label: "React" },
+              { Icon: ApiIcon, label: "API" },
+              { Icon: ThreeIcon, label: "Three JS" },
+            ].map(({ Icon, label }) => (
+              <div
+                key={label}
+                className="border-[.05px] rounded-sm border-[#8f909c] bg-[#111111] flex text-white w-20 h-5 text-xs mx-2"
+              >
+                <div className="w-1/3 flex justify-center text-black items-center h-full bg-[#111111]">
+                  <Icon />
+                </div>
+                <div className="w-2/3 flex justify-center items-center h-full">
+                  {label}
+                </div>
               </div>
-              <div className="w-2/3 flex justify-center items-center h-full">
-                Javascript
-              </div>
-            </div>
-            <div className="border-[.05px] rounded-sm border-[#8f909c] flex text-white bg-[#111111] w-20 h-5 text-xs mx-2">
-              <div className="w-1/3 flex justify-center text-black items-center h-full bg-[#111111]">
-                <ReactIcon />
-              </div>
-              <div className="w-2/3 flex justify-center items-center h-full">
-                React
-              </div>
-            </div>
-
-            <div className="border-[.05px] rounded-sm bg-[#111111] border-[#8f909c] flex text-white w-20 h-5 text-xs mx-2">
-              <div className="w-1/3 flex justify-center bg-[#111111] text-black items-center h-full">
-                <ApiIcon />
-              </div>
-              <div className="w-2/3 flex justify-center items-center h-full">
-                Api
-              </div>
-            </div>
-
-            <div className="border-[.05px] rounded-sm border-[#8f909c] bg-[#111111] flex text-white w-20 h-5 text-xs mx-2">
-              <div className="w-1/3 flex justify-center text-black items-center h-full bg-[#111111]">
-                <ThreeIcon />
-              </div>
-              <div className="w-2/3 flex justify-center items-center h-full">
-                Three Js
-              </div>
-            </div>
+            ))}
           </div>
           <div className="text-[#757a83] flex justify-center items-center text-xs">
-            3D Rendered Models with Three js
+            3D Rendered Models with Three.js
           </div>
         </div>
       </div>
--- src/routes/JavascriptProjects/Pokedex/PokedexBox.jsx
+++ src/routes/JavascriptProjects/Pokedex/PokedexBox.jsx
@@ -31,20 +31,15 @@ export default function PokedexBox() {
       <div className="text-white flex justify-center mt-3">
         JavaScript Projects
       </div>
-      <div
-        className="h-[300px] w-[400px] sm:h-[300px] sm:w-[400px] lg:h-[350px] lg:w-[500px] mt-3 rounded-lg shadow shadow-black border-[1px] border-[#222224]"
-        style={{
-          backgroundSize: "contain",
-          backgroundPosition: "center",
-          backgroundRepeat: "no-repeat",
-        }}
-      >
-        <div className="w-full h-3/5 rounded-lg flex justify-center items-center">
-          {/* Radial Background Effect */}
+      <div className="relative h-[300px] w-[400px] sm:h-[300px] sm:w-[400px] lg:h-[350px] lg:w-[500px] mt-3 rounded-lg shadow shadow-black border-[1px] border-[#222224] overflow-hidden">
+        {/* 3D Model Container */}
+        <div className="relative w-full h-3/5 flex justify-center items-center">
+          {/* Radial Background Effect behind everything */}
           <div
-            className="w-[300px] h-[50px] sm:w-[100px] sm:h-[50px] md:w-[150px] md:h-[80px]  lg:w-[260px]  lg:h-[150px]  rounded-full absolute lg:top-[30%] top-[37%] left-[50%] md:top-[28%] sm:top-[30%] translate-x-[-50%] z-0 translate-y-[-50%] blur-[50px]  pointer-events-none transition-all duration-1000"
+            className="absolute   mt-[90px] flex items-center justify-center w-[290px] h-[100px] pointer-events-none"
             style={{
               background: `radial-gradient(circle at center, ${background} 0%, transparent 100%)`,
+              filter: "blur(70px)",
             }}
           ></div>
 
@@ -69,30 +64,29 @@ export default function PokedexBox() {
           </Canvas>
         </div>
 
-        {/* Tech Stack & Description */}
-        <div className="flex flex-col w-full h-1/5">
+        {/* Project Information */}
+        <div className="flex-col  w-full h-1/5">
           <a
-            href="https://xmiguelcastillo.github.io/pokedex/pikachu"
+            href="https://xmiguelcastillo.github.io/pokedex/bulbasaur"
             target="_blank"
             rel="noopener noreferrer"
-            className="text-white flex z-50 justify-center items-center text-sm hover:underline "
+            className="text-white flex justify-center items-center text-sm hover:underline"
           >
             3D Pokedex
           </a>
-          <div className="flex w-full h-full justify-center items-center">
-            {/* Tech Tags */}
+          <div className="flex w-full h-full  justify-center items-center">
             {[
-              { icon: <JavascriptIcon />, label: "JavaScript" },
-              { icon: <ReactIcon />, label: "React" },
-              { icon: <ApiIcon />, label: "API" },
-              { icon: <ThreeIcon />, label: "Three.js" },
-            ].map(({ icon, label }, index) => (
+              { Icon: JavascriptIcon, label: "Javascript" },
+              { Icon: ReactIcon, label: "React" },
+              { Icon: ApiIcon, label: "API" },
+              { Icon: ThreeIcon, label: "Three JS" },
+            ].map(({ Icon, label }) => (
               <div
-                key={index}
-                className="border-[0.05px] rounded-sm border-[#8f909c] flex text-white bg-[#111111] w-20 h-5 text-xs mx-2"
+                key={label}
+                className="border-[.05px] rounded-sm border-[#8f909c] bg-[#111111] flex text-white w-20 h-5 text-xs mx-2"
               >
                 <div className="w-1/3 flex justify-center text-black items-center h-full bg-[#111111]">
-                  {icon}
+                  <Icon />
                 </div>
                 <div className="w-2/3 flex justify-center items-center h-full">
                   {label}
